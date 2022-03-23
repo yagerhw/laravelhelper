76eab92d116e6f58adaa22fd52946c6c4bce394a
Yager Guo <yagerguo@126.com>
2022-03-23 11:47:58 +0800

    1.0.0

---
+++ src/Models/SlugTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Yager\Laravelhelper\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Yager\Phphelper\Stringhw;
+
+trait SlugTrait
+{
+
+    protected static function bootSlugTrait()
+    {
+        static::creating(function (Model $model) {
+            $model->generateSlug();
+        });
+    }
+
+    protected function generateSlug()
+    {
+        if (!property_exists($this, 'slugField')) {
+            return;
+        }
+
+        $unique = false;
+        $tested = [];
+
+        do {
+            $string = Stringhw::random();
+
+            if (in_array($string, $tested)) {
+                continue;
+            }
+
+            $tested[] = $string;
+
+            $count = DB::table($this->table)->where($this->slugField, '=', $string)->count();
+            if ($count) {
+                continue;
+            }
+
+            $unique = true;
+        } while (!$unique);
+
+        $this->{$this->slugField} = $string;
+    }
+}
